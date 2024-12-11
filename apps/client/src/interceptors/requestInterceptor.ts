67cedd11818054ef0f3be0d767809dacc44d7181
abhi <pandeyabhi142002@gmail.com>
2024-12-11 18:24:49 +0530

    some changes minor

--- apps/client/src/interceptors/requestInterceptor.ts
+++ apps/client/src/interceptors/requestInterceptor.ts
@@ -2,7 +2,7 @@ import { InternalAxiosRequestConfig } from 'axios';
 
 const requestInterceptor = (config: InternalAxiosRequestConfig): InternalAxiosRequestConfig => {
     // const token = localStorage.getItem('authToken');
-    const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjIiLCJpYXQiOjE3MzMwNzE0NDYsImV4cCI6MTczMzUwMzQ0Nn0.8qOXUSureJQ-GSv1VjHvPqCGFV0eNjaF69khFASNkOg"
+    const token = ""
     if (token) {
         config.headers.set('Authorization', `Bearer ${token}`);
     }
